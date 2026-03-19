637467ab027ee5bd03bb7fd97f74d87e0ee6b7c7
Rafo <arafajec@crc.ag>
2026-03-19 18:44:54 +0100

    init

---
+++ src/test/java/hr/tvz/foodiehub/FoodieHubApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tvz.foodiehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodieHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
